08500a0b44feccd8f46b6595fc28c82e6e667d37
MOJA <arinzeorji4@gmail.com>
2018-07-22 15:11:45 +0100

    start script setup

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const path = require('path');
-var app = express();
+const app = express();
 
 const publicPath = path.join(__dirname, '../' );
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
+app.listen(port, () =>{
   console.log(`server is up on port  ${port}`);
 })
