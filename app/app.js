0a4ec63b69707f88a7585f8a5654091cb3d801b5
MOJA <arinzeorji4@gmail.com>
2018-07-23 17:14:11 +0100

    third commit

--- app/app.js
+++ app/app.js
@@ -11,5 +11,5 @@ app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 
 app.listen(app.get('port'), () => {
-    console.log('Server is listening on port ' + app.get('port'));
+    console.log('Server is on port ' + app.get('port'));
 });
